2ec5d1dafdb26a9943113dbf7d45712c0f86c203
chadallen <cga@chadallen.net>
2017-01-12 22:12:39 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# SlackGIF
+Posts GIFs to Slack from AWS IoT button
+
+# AUTHOR
+
+Chad Allen chadallen.net
+
+# DESCRIPTION
+
+Replace humans by posting cat GIFs to Slack
+
+# REQUIREMENTS
+
+* AWS IoT button
+* AWS Lambda
+* Slackbot and associated token
+* API key from http://thecatapi.com/
+
+# HOMEPAGE AND SOURCE REPOSITORY
+
+https://github.com/chadallen/SlackGIF
+
+# THANKS
+
+Made possible by the Cat API from http://twitter.com/adenforshaw  
+
+# TODO
+
+* Add poems (start with this: http://dogepoet.aaroniles.net/api/verse/verse/finale/?mode=alliteration or maybe by scraping this using cat entry on wikipedia: http://webermartin.net/poem.php)  
+
---
+++ postGIF.py
@@ -0,0 +1,35 @@
+from slackclient import SlackClient
+import os
+import urllib2
+import re
+
+slack_token = os.environ["SLACK_BOT_TOKEN"]
+sc = SlackClient(slack_token)
+
+cat_api_token = os.environ["CAT_API_TOKEN"]
+print slack_token
+
+# URL for fetching image html from cat API
+catURL = (
+    'http://thecatapi.com/api/images/get?format=html&api_key=' +
+    cat_api_token +
+    '&type=gif'
+)
+
+# Fetch the image html from cat api
+catImageURL = urllib2.urlopen(catURL)
+catImageHTML = catImageURL.read()
+
+# Get just the image path from image html
+match = re.search(r'src="(.*)">', catImageHTML)
+catImagePath = match.group(1)
+
+print catImagePath
+
+
+sc.api_call(
+    "chat.postMessage",
+    channel="@chadallen",
+    as_user="true",
+    attachments=[{"title": "Cat GIF", "image_url": catImagePath}]
+)
