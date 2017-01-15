432bae375c0bc84f2ffed9a06fb17246ee21f913
chadallen <cga@chadallen.net>
2017-01-14 16:18:04 -0800

    Cleaned up formatting. Added button handler.

---
+++ AWS_IOT_buttonHandler.py
@@ -0,0 +1,3 @@
+import postGIFtoSlack
+
+action = postGIFtoSlack
--- postGIFtoSlack.py
+++ postGIFtoSlack.py
@@ -3,13 +3,14 @@ import os
 import urllib2
 import re
 
+# Set the API token for Slack
 slack_token = os.environ["SLACK_BOT_TOKEN"]
 sc = SlackClient(slack_token)
 
+# Set the API token for The Cat API
 cat_api_token = os.environ["CAT_API_TOKEN"]
-print slack_token
 
-# URL for fetching image html from cat API
+# Build the URL for fetching image html from cat API
 catURL = (
     'http://thecatapi.com/api/images/get?format=html&api_key=' +
     cat_api_token +
@@ -20,17 +21,20 @@ catURL = (
 catImageURL = urllib2.urlopen(catURL)
 catImageHTML = catImageURL.read()
 
-# Get just the image path from image html
+# Strip down the HTML returned by The Cat API to get the image URL
 match = re.search(r'src="(.*)">', catImageHTML)
 catImagePath = match.group(1)
 
-print catImagePath
+# Set Slack channel we're going to post to
+slackChannel = "@chadallen"
 
-
+# Post image URL to slack channel
 sc.api_call(
     "chat.postMessage",
-    channel="@chadallen",
+    channel=slackChannel,
     as_user="true",
     attachments=[{"title": "Cat GIF", "image_url": catImagePath}]
 )
 
+# Cheap debugging
+print catImagePath
