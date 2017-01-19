23000e47b9927bbe311bce38ac1d8a8db2c83fdc
chadallen <cga@chadallen.net>
2017-01-19 15:13:16 -0800

    update variable and function names to match python conventions

--- postGIFtoSlack.py
+++ postGIFtoSlack.py
@@ -4,42 +4,41 @@ import urllib2
 import re
 
 
-
-def postGIFtoSlack():
+def post_GIF_to_slack():
     # Set the API token for Slack
     slack_token = os.environ["SLACK_BOT_TOKEN"]
-    sc = SlackClient(slack_token)
+    slack_client = SlackClient(slack_token)
 
     # Set the API token for The Cat API
     cat_api_token = os.environ["CAT_API_TOKEN"]
 
     # Build the URL for fetching image html from cat API
-    catURL = (
+    cat_URL = (
         'http://thecatapi.com/api/images/get?format=html&api_key=' +
         cat_api_token +
         '&type=gif'
     )
 
     # Fetch the image html from cat api
-    catImageURL = urllib2.urlopen(catURL)
-    catImageHTML = catImageURL.read()
+    cat_image_URL = urllib2.urlopen(cat_URL)
+    cat_image_HTML = cat_image_URL.read()
 
     # Strip down the HTML returned by The Cat API to get the image URL
-    match = re.search(r'src="(.*)">', catImageHTML)
-    catImagePath = match.group(1)
+    match = re.search(r'src="(.*)">', cat_image_HTML)
+    cat_image_path = match.group(1)
 
     # Set Slack channel we're going to post to
-    slackChannel = "@chadallen"
+    slack_channel = "@chadallen"
 
     # Post image URL to slack channel
-    sc.api_call(
+    slack_client.api_call(
         "chat.postMessage",
-        channel=slackChannel,
+        channel=slack_channel,
         as_user="true",
-        attachments=[{"title": "Cat GIF", "image_url": catImagePath}]
+        attachments=[{"title": "Cat GIF", "image_url": cat_image_path}]
     )
 
     # Cheap debugging
-    print catImagePath
+    print cat_image_path
 
-postGIFtoSlack()
+post_GIF_to_slack()
